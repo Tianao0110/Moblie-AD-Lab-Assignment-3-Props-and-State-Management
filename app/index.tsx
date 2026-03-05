b7971c3f2e8630676b404ef9309315d8fdc6c6ed
Tianao0110 <123719125+Tianao0110@users.noreply.github.com>
2026-03-05 16:08:34 -0700

    State Management

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,25 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 import ToDoForm from "./ToDoForm";
 import ToDoList from "./ToDoList";
 
 export default function Index() {
+  const [tasks, setTasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'walk dog'
+  ]);
+
   return(
-    <SafeAreaView style = {{flex: 1}}>
+    <View style={styles.container}>
       <ToDoForm/>
-      <ToDoList/>
-    </SafeAreaView>
+      <ToDoList tasks = {tasks}/>
+    </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+  }
+});
