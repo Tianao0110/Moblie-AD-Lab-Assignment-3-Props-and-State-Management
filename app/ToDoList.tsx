7225308d6532d5342b3c621e395ea0ef6d5e1767
Tianao0110 <123719125+Tianao0110@users.noreply.github.com>
2026-03-05 16:08:45 -0700

    Passing Props

--- app/ToDoList.tsx
+++ app/ToDoList.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import { ScrollView, Pressable, View, Text, StyleSheet } from 'react-native';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 
-function ToDoList() {
+function ToDoList({ tasks }: { tasks: string[] }) {
   return (
     <ScrollView>
-      <Pressable>
-        <View style={[styles.task, styles.completed]}>
-          <Text style={styles.taskText}>Do laundry</Text>
-        </View>
-      </Pressable>
-      <Pressable>
-        <View style={[styles.task]}>
-          <Text style={styles.taskText}>Go to gym</Text>
-        </View>
-      </Pressable>
-      <Pressable>
-        <View style={[styles.task, styles.completed]}>
-          <Text style={styles.taskText}>Walk dog</Text>
-        </View>
-      </Pressable>
+      {tasks.map((task, index) => (
+        <Pressable key={index}>
+          <View style={[styles.task]}>
+            <Text style={styles.taskText}>{task}</Text>
+          </View>
+        </Pressable>
+      ))}
     </ScrollView>
   );
 }
